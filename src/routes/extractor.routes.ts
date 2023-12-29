331e188a289e5a4ad6ff2a45d4d4ba0968102aa6
KillerDogs <kenybustamante2@hotmail.com>
2023-12-29 13:40:57 -0300

    remove log

--- src/routes/extractor.routes.ts
+++ src/routes/extractor.routes.ts
@@ -5,7 +5,6 @@ const router = express.Router()
 
 router.post("/", async (req, res) => {
   try {
-    console.log(req.body)
     const { url, amazon, magazine } = req.body
 
     const result = await extractorController.extractMetadata(
